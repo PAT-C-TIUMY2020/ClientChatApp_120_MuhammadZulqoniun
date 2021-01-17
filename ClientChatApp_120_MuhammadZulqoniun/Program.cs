8de7706d413f3d2ed3d25e8f1ff854102e3575a7
Zulqoniun <muhammadqoni123@gmail.com>
2021-01-17 18:41:05 +0700

    Add project files.

---
+++ ClientChatApp_120_MuhammadZulqoniun/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ClientChatApp_120_MuhammadZulqoniun
+{
+    public class ClientCallback : ServiceReference1.IServiceCallbackCallback //sebagai context/channel
+    {
+        public void pesanKirim(string user, string pesan)
+        {
+            Console.WriteLine("{0}: {1}", user, pesan);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallback());
+            ServiceReference1.ServiceCallbackClient server = new ServiceReference1.ServiceCallbackClient(context);
+
+            Console.WriteLine("Enter Username");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim Pesan");
+            string pesan = Console.ReadLine();
+
+            //memeriksa apakah pesannya null
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan))
+                    server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan");
+                pesan = Console.ReadLine();
+            }
+        }
+    }
+}
